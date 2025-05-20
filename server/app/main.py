272a305d83a793c566d7f21aaa5715d32b0cf77b
Carlos Alberto Sánchez <96161558+Elcasvi@users.noreply.github.com>
2025-05-20 03:44:39 -0600

    Refactor to request to fix issue endpoint in extension

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
 from app.routers.AgentRoutes import router
 
 app = FastAPI(title="CodeMedic API")
 
 app.include_router(router, prefix="/api")
+
+# orig_origins = [
+#     "http://localhost:4200",
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=orig_origins,  # List of allowed origins
+#     allow_credentials=True,
+#     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+#     allow_headers=["*"]  # Allow all headers
+# )
 
 @app.get("/")
 async def root():
